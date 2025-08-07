4249236a6a2e852fdcca6a59a439f688e702188d
habibajavaid <habibajavaid17@gmail.com>
2025-08-07 08:11:19 -0700

    Add files via upload

---
+++ GameInterface.java
@@ -0,0 +1,18 @@
+
+/*@Habiba Javaid
+Roll No:0030.
+*/
+
+
+
+
+
+
+
+//This interface is for the overall game flow.
+public interface GameInterface {
+    void startGame(); //this is used for the start of game.
+    void nextLevel(); //this is used when we move to the next level of the game.
+    void endGame();   //this is used for the end of game.
+}
+ 
